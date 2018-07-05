ebb2155b73af5b235a4d3929d4a6714461f748c1
ZSCDumin <2712220318@qq.com>
2018-07-05 17:40:25 +0800

    更新代码

--- src/FactoryPatternDemo.java
+++ src/FactoryPatternDemo.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class FactoryPatternDemo {
 	public static void main(String[] args) {
@@ -5,24 +6,23 @@ public class FactoryPatternDemo {
 		double area = 0.0;
 		double perimer = 0.0;
 
+		Scanner sc = new Scanner(System.in);
 		ShapeFactory shapeFactory = new ShapeFactory();
 		Circle circle = (Circle) shapeFactory.getShape("Circle");
-		circle.setR(2.0);
+		circle.setR(sc.nextDouble());
 		area = circle.getArea();
 		perimer = circle.getPerimeter();
 		System.out.println(area + "---" + perimer);
 
-
 		Square square = (Square) shapeFactory.getShape("Square");
-		square.setA(2.0);
+		square.setA(sc.nextDouble());
 		area = square.getArea();
 		perimer = square.getPerimeter();
 		System.out.println(area + "---" + perimer);
 
-
 		Rectangle rectangle = (Rectangle) shapeFactory.getShape("Rectangle");
-		rectangle.setA(2.0);
-		rectangle.setB(2.0);
+		rectangle.setA(sc.nextDouble());
+		rectangle.setB(sc.nextDouble());
 		area = rectangle.getArea();
 		perimer = rectangle.getPerimeter();
 		System.out.println(area + "---" + perimer);
